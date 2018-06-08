aae8e930f67aa621279b983a5075643d45887349
Kunal Jain <jain.kunal35@ymail.com>
2018-06-09 02:33:28 +0800

    Completed Main added nms. Added coco file

--- main.py
+++ main.py
@@ -9,16 +9,17 @@ import numpy as np
 import cv2
 import argparse
 
-from torchDarknet import DarknetTorch
+from torchDarknet import DarknetTorch, nmsYOLO
 
 def parser():
 	ap = argparse.ArgumentParser()
-	ap.add_argument("-v","--video", dest='vpath', help="Input Video for person detection")
-	ap.add_argument("-i","--img", dest='imgpath', help="Input Image for person detection")
+	ap.add_argument("-v","--video", dest='video', help="Input Video for person detection")
+	# ap.add_argument("-i","--img", dest='imgpath', help="Input Image Folder for person detection")
+	# ap.add_argument("-o","--out", dest='outpath', help="Output Folder for person detection")
 	ap.add_argument("-tc","--confthres", dest='confthres', help = "Object Confidence to filter predictions", default=0.5)
 	ap.add_argument("-iou","--nmsthres", dest = "nmsthres", help = "NMS IOU Threshhold", default = 0.4)
-	ap.add_argument("-c","--cfg", dest='cfgFile',help="Yolov3 Darknet Config File", default="./cfg/yolov3.cfg")
-	ap.add_argument("-w","--weights", dest='wPath',help="Yolov3 Pretrained Weights File", default="./yolov3.weights")
+	ap.add_argument("-c","--cfg", dest='cfg',help="Yolov3 Darknet Config File", default="./cfg/yolov3.cfg")
+	ap.add_argument("-w","--weights", dest='weights',help="Yolov3 Pretrained Weights File", default="./yolov3.weights")
 	ap.add_argument("-r","--res", dest='res', help="Input resolution of the network. Increase to increase accuracy. Decrease to increase speed", default="416", type=str)
 	return vars(ap.parse_args())
 
@@ -30,7 +31,7 @@ def preprocess(orig_img, dim):
 	ow, oh = orig_dim
 	ratio = min(w/ow,h/oh)
 	wnew,hnew = int(ow*ratio),int(oh*ratio)
-	img_resized = cv2.resize(img,(wnew,hnew),interpolation=cv2.INTER_CUBIC)
+	img_resized = cv2.resize(orig_img,(wnew,hnew),interpolation=cv2.INTER_CUBIC)
 	img = np.full((h,w,3),128)
 	img[(h-hnew)//2:hnew+(h-hnew)//2,(w-wnew)//2:wnew+(w-wnew)//2,  :] = img_resized
 
@@ -40,8 +41,15 @@ def preprocess(orig_img, dim):
 
 	return(img_,orig_img,orig_dim)
 
-
-
+def mark_classes(x,orig_img):
+	label = class_names[int(x[-1])]
+	if label=="person":
+		cv2.rectangle(orig_img,tuple(x[1:3].int()),tuple(x[3:5].int()),(0,255,0))
+		font_sz = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 1 , 1)[0]  
+		txtcorner = tuple(x[1:3].int())
+		txtcorner = txtcorner[0]+font_sz[0]+2,txtcorner[1]+font_sz[1]+2,
+		cv2.rectangle(orig_img,tuple(x[1:3].int()),txtcorner,(255,0,0))
+		cv2.putText(orig_img,label,(x[1].int(),txtcorner[1]), cv2.FONT_HERSHEY_PLAIN,1,(255,255,255))
 
 if __name__=="__main__":
 	args = parser()
@@ -50,6 +58,10 @@ if __name__=="__main__":
 
 	CUDA = torch.cuda.is_available()
 	device = torch.device("cuda" if CUDA else "cpu")
+
+	num_classes = 80
+	fclasses = open("./data/coco.names","r")
+	class_names = fclasses.read().split("\n")[:-1]
 
 	print("Loading network...")
 	YOLO = DarknetTorch(args["cfg"])
@@ -61,37 +73,76 @@ if __name__=="__main__":
 	# Decreased resolution means faster evaluation.
 	YOLO.netparams["height"] = str(args["res"])
 	dim = int(YOLO.netparams["height"])
-	assert dim % 32 == 0 
-	assert dim > 32
+	assert dim % 32 == 0 and dim > 32
 
 	if CUDA:
 		print("Running on GPU.")	
 	else: 
 		print("No GPU found. Running on CPU")
-	YOLO.to(device)
+	YOLO = YOLO.to(device)
 
+	# Specify testing
+	YOLO.eval()
 
 	if args["video"]:
 		cap = cv2.VideoCapture(args["video"])
 		assert cap.isOpened(), 'Cannot open source'
 
-		i=0
+		currFrame=0
 		start = time.time()
 		while cap.isOpened():
 			_,frame = cap.read()
-			img,orig_img,orig_dim = preprocess(frame,dim)
+			img,orig_img,orig_dim = preprocess(frame,(dim,dim))
 			
-			# orig_dim = torch.FloatTensor(orig_dim).repeat(1,2)
 			orig_dim = torch.FloatTensor(orig_dim).to(device)
 			img = img.to(device)
 
 			with torch.no_grad():
-				output = net(Variable(img), CUDA)
-			
+				output = YOLO(Variable(img), CUDA)
+
+			output = nmsYOLO(output, device, confthres, num_classes, nmsthres)	#####################
+
+			# continue loop if no detections
+			if type(output) == int:
+				currFrame += 1
+				print("FPS of the video is %f."%( currFrame / (time.time() - start)))
+				cv2.imshow("frame", orig_img)
+				key = cv2.waitKey(1)
+				if key & 0xFF == ord('q'):
+				    break
+				continue
+
+			# ratio = torch.min(dim/orig_dim)[0]	########## TORCH 0.5 WARNING
+
+			# #scale output
+			# output[:,[1,3]] = (output[:,[1,3]] - (dim - ratio*orig_dim[0])/2)/ratio
+			# output[:,[2,4]] = (output[:,[1,3]] - (dim - ratio*orig_dim[1])/2)/ratio
+
+			# for i in range(output.shape[0]):
+			# 	output[i,[1,3]] = torch.clamp(output[i,[1,3]], 0.0, orig_dim[0])
+			# 	output[i,[2,4]] = torch.clamp(output[i,[2,4]], 0.0, orig_dim[1])
+
+			orig_dim = orig_dim.repeat(output.size(0), 1)
+			scaling_factor = torch.min(dim/orig_dim,1)[0].view(-1,1)
+
+			output[:,[1,3]] -= (dim - scaling_factor*orig_dim[:,0].view(-1,1))/2
+			output[:,[2,4]] -= (dim - scaling_factor*orig_dim[:,1].view(-1,1))/2
+
+			output[:,1:5] /= scaling_factor
+
+			for i in range(output.shape[0]):
+				output[i, [1,3]] = torch.clamp(output[i, [1,3]], 0.0, orig_dim[i,0])
+				output[i, [2,4]] = torch.clamp(output[i, [2,4]], 0.0, orig_dim[i,1])
 
 
+			list(map(lambda x: mark_classes(x,orig_img),output))
+
+			#show video frame
+			cv2.imshow("frame", orig_img)
+			key = cv2.waitKey(1)
+			if key & 0xFF == ord('q'):
+				break
+			currFrame += 1
+			print("FPS of the video is %f."%( currFrame / (time.time() - start)))
 
 
-
-
-	YOLO()
--- torchDarknet.py
+++ torchDarknet.py
@@ -44,59 +44,172 @@ class DummyLayer(nn.Module):
 # Define custom module class for yolo detection. 
 # Just a dummy module to store anchors while initializing net.
 class YOLODummyLayer(nn.Module):
-    def __init__(self, anchors):
-        super(YOLODummyLayer, self).__init__()
-        self.anchors = anchors
+	def __init__(self, anchors):
+		super(YOLODummyLayer, self).__init__()
+		self.anchors = anchors
 
-    # def forward(self, x, imgsz, num_classes, confidence):
-    #     x = x.data
-    #     global CUDA
-    #     pred = x
-    #     pred = predictYOLO(pred, imgsz, self.anchors, num_classes, confidence, CUDA)
-    #     return pred
 
 def predictYOLO(pred, imgsz, anchors, num_classes = 80, CUDA = True):
-    batch_sz = pred.size(0)
-    stride =  imgsz // pred.size(2)
-    grid_sz = imgsz // stride
-    num_anchors = len(anchors)  #3
+	batch_sz = pred.size(0)
+	stride =  imgsz // pred.size(2)
+	grid_sz = imgsz // stride
+	num_anchors = len(anchors)  #3
 
-    #scale anchor boxes
-    anchors = [(a[0]/stride, a[1]/stride) for a in anchors]
+	#scale anchor boxes
+	anchors = [(a[0]/stride, a[1]/stride) for a in anchors]
 
-    #transform pred tensor into output form
-    pred = pred.view(batch_sz, (5 + num_classes)*num_anchors, grid_sz*grid_sz)
-    pred = pred.transpose(1,2).contiguous() #view can work only on memory contiguous tensors
-    pred = pred.view(batch_sz, grid_sz*grid_sz*num_anchors, (5 + num_classes))
+	#transform pred tensor into output form
+	pred = pred.view(batch_sz, (5 + num_classes)*num_anchors, grid_sz*grid_sz)
+	pred = pred.transpose(1,2).contiguous() #view can work only on memory contiguous tensors
+	pred = pred.view(batch_sz, grid_sz*grid_sz*num_anchors, (5 + num_classes))
 
-    #tx, ty, pc are sigmoided and center offsets are added
-    pred[:,:,0] = torch.sigmoid(pred[:,:,0])
-    pred[:,:,1] = torch.sigmoid(pred[:,:,1])
-    pred[:,:,4] = torch.sigmoid(pred[:,:,4])
+	#tx, ty, pc are sigmoided and center offsets are added
+	pred[:,:,0] = torch.sigmoid(pred[:,:,0])
+	pred[:,:,1] = torch.sigmoid(pred[:,:,1])
+	pred[:,:,4] = torch.sigmoid(pred[:,:,4])
 
-    grid_len = np.arange(grid_sz)
-    a,b = np.meshgrid(grid_len, grid_len)
-    x_offset = torch.FloatTensor(a).view(-1,1)
-    y_offset = torch.FloatTensor(b).view(-1,1)
-    if CUDA:
-        x_offset = x_offset.cuda()
-        y_offset = y_offset.cuda()
-    x_y_offset = torch.cat((x_offset, y_offset), 1).repeat(1,num_anchors).view(-1,2).unsqueeze(0)
-    pred[:,:,:2] += x_y_offset
+	grid_len = np.arange(grid_sz)
+	a,b = np.meshgrid(grid_len, grid_len)
+	x_offset = torch.FloatTensor(a).view(-1,1)
+	y_offset = torch.FloatTensor(b).view(-1,1)
+	if CUDA:
+		x_offset = x_offset.cuda()
+		y_offset = y_offset.cuda()
+	x_y_offset = torch.cat((x_offset, y_offset), 1).repeat(1,num_anchors).view(-1,2).unsqueeze(0)
+	pred[:,:,:2] += x_y_offset
 
-    #scale anchors
-    anchors = torch.FloatTensor(anchors)
-    if CUDA:
-        anchors = anchors.cuda()
-    anchors = anchors.repeat(grid_sz*grid_sz, 1).unsqueeze(0)
-    pred[:,:,2:4] = torch.exp(pred[:,:,2:4])*anchors
+	#scale anchors
+	anchors = torch.FloatTensor(anchors)
+	if CUDA:
+		anchors = anchors.cuda()
+	anchors = anchors.repeat(grid_sz*grid_sz, 1).unsqueeze(0)
+	pred[:,:,2:4] = torch.exp(pred[:,:,2:4])*anchors
 
-    #sigmoid class scores
-    pred[:,:,5:5+num_classes] = torch.sigmoid((pred[:,:, 5:5+num_classes]))
+	#sigmoid class scores
+	pred[:,:,5:5+num_classes] = torch.sigmoid((pred[:,:, 5:5+num_classes]))
 
-    pred[:,:,:4] *= stride
-    
-    return pred
+	pred[:,:,:4] *= stride
+
+	return pred
+
+
+def IOU(box1, box2):
+	"""
+	Calculates the IoU of two bounding boxes 
+	"""
+
+	#Get bounding box corners
+	b1_x1, b1_y1, b1_x2, b1_y2 = box1[:,0], box1[:,1], box1[:,2], box1[:,3]
+	b2_x1, b2_y1, b2_x2, b2_y2 = box2[:,0], box2[:,1], box2[:,2], box2[:,3]
+
+	#Intersection(n) corners
+	nx1 =  torch.max(b1_x1, b2_x1)
+	ny1 =  torch.max(b1_y1, b2_y1)
+	nx2 =  torch.min(b1_x2, b2_x2)
+	ny2 =  torch.min(b1_y2, b2_y2)
+
+	#Intersection(n) area
+	n_area = torch.clamp(nx2-nx1+1, min=0)*torch.clamp(ny2-ny1+1, min=0)
+
+	#Rectangle Areas 
+	b1_area = (b1_x2 - b1_x1+1)*(b1_y2 - b1_y1+1)
+	b2_area = (b2_x2 - b2_x1+1)*(b2_y2 - b2_y1+1)
+
+	# IOU = Intersection Over Union = Intersection/Union
+	iou = n_area / (b1_area + b2_area - n_area)
+
+	return iou
+
+def nmsYOLO(pred, device, confthres = 0.5, num_classes = 80, nmsthres = 0.4):
+	"""
+	Input: (batchsz x bounding boxes x bbox attributes) torch tensor
+	Output: (nms bounding boxes x 8) torch tensor
+	8 values: Batch Idx(0),bounding box coordinates (1,2,3,4),
+			  object conf(5), max class prob (6), object label (7)
+	"""
+	batch_sz = pred.size(0)
+
+	conf_mask = (pred[:,:,4]>confthres).float().unsqueeze(2)
+	
+	# Return if there is no confident prediction.
+	if len(torch.nonzero(conf_mask))==0:
+		return 0
+
+	pred = pred*conf_mask
+	# nzpred = torch.nonzero(pred[:,:,4])
+	# pred = pred[]
+
+	# using center and box lengths, get topleft and bottomright corners
+	# which will be easier to plot using opencv rectangle function
+	corners = pred.new(pred[:,:,:4].shape)
+	corners[:,:,0] = (pred[:,:,0] - pred[:,:,2]/2)
+	corners[:,:,1] = (pred[:,:,1] - pred[:,:,3]/2)
+	corners[:,:,2] = (pred[:,:,0] + pred[:,:,2]/2) 
+	corners[:,:,3] = (pred[:,:,1] + pred[:,:,3]/2)
+	pred[:,:,:4] = corners[:,:,:4]
+
+	output = []
+	# loop over all images in the given batch to get nms scores and new predictions
+	for ib in range(batch_sz):
+		imgPred = pred[ib]
+
+		maxConf, maxClass= torch.max(imgPred[:,5:5+num_classes],1)
+		maxConf = maxConf.float().unsqueeze(1)
+		maxClass = maxClass.float().unsqueeze(1)
+		imgPred = torch.cat((imgPred[:,:5],maxConf,maxClass),1)
+
+		# Select only non-zero predictions
+		nzpos =  (torch.nonzero(imgPred[:,4])).squeeze()
+		imgPred = imgPred[nzpos,:].view(-1,7)
+
+		try:
+			npPred = imgPred[:,-1].cpu().numpy()
+			predClasses = torch.from_numpy(np.unique(npPred))
+			predClasses = predClasses.to(device)
+		except:
+			continue
+
+
+		# Perform nms wrt person class and remove all other BBs
+		PERSON = 0.0
+		for class_ in predClasses:
+			if PERSON != class_.cpu().numpy():
+				continue
+			else:
+				predPerson = imgPred*(class_==imgPred[:,-1]).float().unsqueeze(1)
+				nzmask = torch.nonzero(predPerson[:,-2]).squeeze()
+
+				imgPred = imgPred[nzmask].view(-1,7)
+
+				# sort detections in descending objectness confidence(4)
+				confIdx = torch.sort(imgPred[:,4], descending=True)[1]
+				imgPred = imgPred[confIdx]
+
+				for i in range(imgPred.size(0)):
+					try:
+						iou = IOU(imgPred[i].unsqueeze(0),imgPred[i+1:])
+					except:
+						break
+
+					ioumask = (iou < nmsthres).float().unsqueeze(1)
+					imgPred[i+1:] = imgPred[i+1:]*ioumask
+					nzmask = torch.nonzero(imgPred[:,4]).squeeze()
+					imgPred = imgPred[nzmask].view(-1,7)
+
+
+				batchIdx = imgPred.new(imgPred.size(0), 1).fill_(ib)
+
+				out = torch.cat((batchIdx,imgPred),1)
+				if isinstance(output, list):
+					output = out
+				else:
+					output	= torch.cat((output,out))
+
+				break
+	if isinstance(output,list):
+		return 0
+	return output
+
 
 def module_define_torch(sections):
 	"""
@@ -228,7 +341,7 @@ class DarknetTorch(nn.Module):
 				else:
 					detects	= torch.cat((detects,x),1)	#concatenate along channel depth
 				outputs[i] = outputs[i-1]
-		print(detects)
+		# print(detects)
 		return detects
 
 
@@ -288,25 +401,25 @@ class DarknetTorch(nn.Module):
 # secs = parse_Darknet_cfg("./cfg/yolov3.cfg")
 # module_define_torch(secs)
 
-def get_test_input(input_dim, CUDA):
-    img = cv2.imread("dog-cycle-car.png")
-    img = cv2.resize(img, (input_dim, input_dim)) 
-    img_ =  img[:,:,::-1].transpose((2,0,1))
-    img_ = img_[np.newaxis,:,:,:]/255.0
-    img_ = torch.from_numpy(img_).float()
-    img_ = Variable(img_)
+# def get_test_input(input_dim, CUDA):
+#     img = cv2.imread("dog-cycle-car.png")
+#     img = cv2.resize(img, (input_dim, input_dim)) 
+#     img_ =  img[:,:,::-1].transpose((2,0,1))
+#     img_ = img_[np.newaxis,:,:,:]/255.0
+#     img_ = torch.from_numpy(img_).float()
+#     img_ = Variable(img_)
     
-    if CUDA:
-        img_ = img_.cuda()
-    return img_
+#     if CUDA:
+#         img_ = img_.cuda()
+#     return img_
 
-CUDA = torch.cuda.is_available()
-print(CUDA)
-dn = DarknetTorch('cfg/yolov3.cfg')
-dn.loadWeights("yolov3.weights")
-inp = get_test_input(320, CUDA)
-print(inp)
-if CUDA:
-    dn.cuda()
-dn(inp,CUDA)
-dn.eval()
+# CUDA = torch.cuda.is_available()
+# print(CUDA)
+# dn = DarknetTorch('cfg/yolov3.cfg')
+# dn.loadWeights("yolov3.weights")
+# inp = get_test_input(320, CUDA)
+# print(inp)
+# if CUDA:
+#     dn.cuda()
+# dn(inp,CUDA)
+# dn.eval()
